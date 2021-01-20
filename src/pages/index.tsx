9d3a800567763886d1475aec8648a8127e3689a4
Vien Dinh <maxvien@gmail.com>
2021-01-20 18:19:16 +0700

    use env service

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { EnvService } from '@shared/services/env.service';
+
 export default function Page() {
+  console.log('isBrowser:', EnvService.isBrowser());
+  console.log('isNode:', EnvService.isNode());
+  console.log('isProd:', EnvService.isProd());
+  console.log('isDev:', EnvService.isDev());
+  console.log('isTest:', EnvService.isTest());
+  console.log('get:', EnvService.get('NEXT_PUBLIC_VARIABLE'));
+
   return <h1>Hello World!</h1>;
 }
