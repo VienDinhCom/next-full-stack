fbce2fb0522f16479cb73569444343fec614f51c
Vien Dinh <maxvien@gmail.com>
2021-01-26 18:01:06 +0700

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import { EnvService } from '@shared/services/env.service';
+import { ApiService } from '@frontend/services/api.service';
 
 export default function Page() {
-  console.log('isBrowser:', EnvService.isBrowser());
-  console.log('isNode:', EnvService.isNode());
-  console.log('isProd:', EnvService.isProd());
-  console.log('isDev:', EnvService.isDev());
-  console.log('isTest:', EnvService.isTest());
-  console.log('get:', EnvService.get('NEXT_PUBLIC_VARIABLE'));
+  const { loading, data, error } = ApiService.useQuery(({ message }) => {
+    return message.hello({ name: 'Vien' });
+  });
 
-  return <h1>Hello World!</h1>;
+  if (loading) return <p>Loading ...</p>;
+
+  if (error) return <p>Error: {error.message}</p>;
+
+  return <h1>{data}</h1>;
 }
