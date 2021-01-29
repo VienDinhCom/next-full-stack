c065be7065ec118d94eb4411bd530d00018d150c
Vien Dinh <maxvien@gmail.com>
2021-01-29 18:20:14 +0700

    Put FileResolver into RootResolver

--- src/backend/resolvers/root.resolver.ts
+++ src/backend/resolvers/root.resolver.ts
@@ -1,11 +1,13 @@
 import { Context, createRootResolver } from '@backend/core';
 import { RootSchema } from '@shared/schemas/root.schema';
 
+import { FileResolver } from './file.resolver';
 import { MessageResolver } from './message.resolver';
 
 export const RootResolver = createRootResolver({
   RootSchema,
   resolvers: {
+    file: FileResolver,
     message: MessageResolver,
   },
   context: async (req, res): Promise<Context> => {
