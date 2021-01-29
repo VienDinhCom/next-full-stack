0b9e7106b72f2ffb250bc17e99979b107f153e3b
Vien Dinh <maxvien@gmail.com>
2021-01-29 18:19:45 +0700

    Put FileSchema into RootSchema

--- src/shared/schemas/root.schema.ts
+++ src/shared/schemas/root.schema.ts
@@ -1,7 +1,9 @@
 import { createRootSchema } from 'soki';
 
+import { FileSchema } from './file.schema';
 import { MessageSchema } from './message.schema';
 
 export const RootSchema = createRootSchema({
+  file: FileSchema,
   message: MessageSchema,
 });
