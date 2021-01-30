469a684537ec3716d335d4972cbb52cf307f7457
Vien Dinh <maxvien@gmail.com>
2021-01-30 21:42:37 +0700

    Fix api service

--- src/frontend/services/api.service.ts
+++ src/frontend/services/api.service.ts
@@ -5,9 +5,12 @@ import { EnvService } from '@shared/services/env.service';
 export type { File } from 'soki/client';
 export { useQuery, useMuation, getFiles } from 'soki/client';
 
+const clientEndpoint = '/api';
+const serverEndpoint = `http${EnvService.isProd() ? 's' : ''}://${EnvService.get('HOST')}/api`;
+
 export const ApiService = createClient({
   RootSchema,
-  endpoint: '/api',
+  endpoint: EnvService.isBrowser() ? clientEndpoint : serverEndpoint,
   options: {
     onRequest: async () => {
       return {
