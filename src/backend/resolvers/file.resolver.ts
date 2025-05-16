c42b6df4531eb484a0585ebb9ba57e663af340f5
Vien Dinh <maxvien@gmail.com>
2025-05-16 01:52:10 +0000

    start over with astro

--- src/backend/resolvers/file.resolver.ts
+++
@@ -1,7 +0,0 @@
-import { Resolvers, createResolver } from '@backend/core';
-
-export const FileResolver = createResolver<Resolvers['file']>({
-  upload: async ({ file }, context) => {
-    return file.name;
-  },
-});
--- src/backend/resolvers/message.resolver.ts
+++
@@ -1,7 +0,0 @@
-import { Resolvers, createResolver } from '@backend/core';
-
-export const MessageResolver = createResolver<Resolvers['message']>({
-  hello: async ({ name }, context) => {
-    return `Hello ${name}!`;
-  },
-});
