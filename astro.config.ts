a6f72129af6d53932ea23bb9457525b168241ded
Vien Dinh <maxvien@gmail.com>
2025-05-16 03:44:42 +0000

    astro: add react support

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,17 @@
+import react from '@astrojs/react'
+
 import { defineConfig, envField } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  server: { port: 3000 },
+  integrations: [react()],
   env: {
     schema: {
       DATABASE_URL: envField.string({ context: 'server', access: 'secret' }),
+      BETTER_AUTH_SECRET: envField.string({ context: 'server', access: 'secret' }),
+      BETTER_AUTH_URL: envField.string({ context: 'server', access: 'secret' }),
     },
   },
+
 })
