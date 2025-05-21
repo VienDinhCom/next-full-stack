e7431359529046ad0b756e1d51b127d5079b04fe
Vien Dinh <maxvien@gmail.com>
2025-05-21 05:19:05 +0000

    shadcn: fix build scripts

---
+++ packages/shadcn/shadcn.ts
@@ -0,0 +1,44 @@
+import fs from "node:fs";
+import path from "node:path";
+import { spawnSync } from "node:child_process";
+import { globbySync } from "globby";
+
+const commands = {
+  rm: (path: string) => {
+    fs.rmSync(path, { recursive: true, force: true });
+  },
+  exec: (command: string) => {
+    spawnSync(command, { shell: true, stdio: "inherit", env: process.env });
+  },
+};
+
+(function shadcn() {
+  commands.rm("src/hooks");
+  commands.rm("src/components");
+
+  commands.exec("npx shadcn add --all");
+
+  let index = "";
+
+  globbySync("src/hooks/*.ts").forEach(
+    (p) => {
+      const { dir, name } = path.parse(p);
+      const filePath = path.join(dir.replace("src", "."), name);
+
+      index += `export * from "./${filePath}";\n`;
+    },
+  );
+
+  index += "\n";
+
+  globbySync("src/components/ui/*.tsx").forEach(
+    (p) => {
+      const { dir, name } = path.parse(p);
+      const filePath = path.join(dir.replace("src", "."), name);
+
+      index += `export * from "./${filePath}";\n`;
+    },
+  );
+
+  fs.writeFileSync("src/index.ts", index);
+})();
