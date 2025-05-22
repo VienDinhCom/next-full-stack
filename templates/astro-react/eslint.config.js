b7b7c6c2cd74f48f13fbd34e580761fc40dfc6ff
Vien Dinh <maxvien@gmail.com>
2025-05-22 12:23:23 +0000

    astro-react: config eslint

--- templates/astro-react/eslint.config.js
+++ templates/astro-react/eslint.config.js
@@ -5,6 +5,5 @@ export default defineConfig({
   type: "app",
   astro: true,
   react: true,
-  formatters: "prettier",
   ignores: ["src/shared/components/ui/*", "src/shared/database/migrations"],
 });
