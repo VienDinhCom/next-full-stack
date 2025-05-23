f97a5c2b6fce3e5cc2cf8f384efe393c0cc0b89e
Vien Dinh <maxvien@gmail.com>
2025-05-23 07:54:03 +0000

    eslint: config antfu to work with prettier

--- packages/eslint/eslint.config.js
+++ packages/eslint/eslint.config.js
@@ -1,11 +1,8 @@
+// @ts-check
 import antfu from "@antfu/eslint-config";
 
 export default antfu({
-  typescript: true,
-  formatters: true,
-  stylistic: {
-    indent: 2,
-    semi: true,
-    quotes: "double",
-  },
+  type: "lib",
+  stylistic: false,
+  ignores: [],
 });
