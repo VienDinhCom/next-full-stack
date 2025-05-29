1182c3a786ea599e15de9888750afa884900a6a6
Vien Dinh <maxvien@gmail.com>
2025-05-29 05:33:06 +0000

    create: rename project after creating

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -2,8 +2,10 @@
 
 import { defineCommand, runMain } from "citty";
 import { consola } from "consola";
+import { omit } from "es-toolkit";
 import { downloadTemplate } from "giget";
 import fs from "node:fs";
+import path from "node:path";
 
 export const meta = (() => {
   const pkgPath = new URL("../package.json", import.meta.url);
@@ -38,14 +40,17 @@ const main = defineCommand({
 
       if (template) {
         await downloadTemplate(`github:VienDinhCom/esmate/templates/${template}`, { dir: name });
+      } else {
+        template = await consola.prompt("Select a template: ", { type: "select", options: templates });
+        name = await consola.prompt("Name your project: ", { type: "text" });
 
-        return;
+        await downloadTemplate(`github:VienDinhCom/esmate/templates/${template}`, { dir: name });
       }
 
-      template = await consola.prompt("Select a template: ", { type: "select", options: templates });
-      name = await consola.prompt("Name your project: ", { type: "text" });
+      const packageJsonPath = path.join(name, "package.json");
+      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
 
-      await downloadTemplate(`github:VienDinhCom/esmate/templates/${template}`, { dir: name });
+      fs.writeFileSync(packageJsonPath, JSON.stringify({ name, ...omit(packageJson, ["name"]) }));
     } catch (error) {
       consola.error(error);
     }
