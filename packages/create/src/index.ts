e73e87d0e7a6b30be643d131d6eb92b76340be4f
Vien Dinh <maxvien@gmail.com>
2025-06-09 09:43:50 +0000

    create: update post messages

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -2,10 +2,11 @@
 
 import { defineCommand, runMain } from "citty";
 import { consola } from "consola";
-import { omit } from "es-toolkit";
+import { kebabCase, omit } from "es-toolkit";
 import { downloadTemplate } from "giget";
 import fs from "node:fs";
 import path from "node:path";
+import process from "node:process";
 
 export const meta = (() => {
   const pkgPath = new URL("../package.json", import.meta.url);
@@ -34,7 +35,7 @@ const main = defineCommand({
   async run({ args }) {
     try {
       let template = args.template;
-      let name = args.name || args.template;
+      let name = kebabCase(args.name || args.template);
 
       const templates = ["react-spa", "astro-react"];
 
@@ -42,7 +43,7 @@ const main = defineCommand({
         await downloadTemplate(`github:VienDinhCom/esmate/templates/${template}`, { dir: name });
       } else {
         template = await consola.prompt("Select a template: ", { type: "select", options: templates });
-        name = await consola.prompt("Name your project: ", { type: "text", default: template });
+        name = kebabCase(await consola.prompt("Name your project: ", { type: "text", default: template }));
 
         await downloadTemplate(`github:VienDinhCom/esmate/templates/${template}`, { dir: name });
       }
@@ -51,6 +52,13 @@ const main = defineCommand({
       const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
 
       fs.writeFileSync(packageJsonPath, JSON.stringify({ name, ...omit(packageJson, ["name"]) }, null, 2));
+
+      consola.info(`\nScaffolding project in ${path.join(process.cwd(), name)}...`);
+      consola.info(`\nDone. Now run:\n`);
+
+      consola.log(`cd ${name}`);
+      consola.log(`npm install`);
+      consola.log(`npm run dev`);
     } catch (error) {
       consola.error(error);
     }
