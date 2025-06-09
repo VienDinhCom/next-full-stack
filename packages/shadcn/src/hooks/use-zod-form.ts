0818a8e256d3a2186f368c049021173ed01c5a39
Vien Dinh <maxvien@gmail.com>
2025-06-09 10:43:08 +0000

    shadcn: upgrade and fix type errors

--- packages/shadcn/src/hooks/use-zod-form.ts
+++ packages/shadcn/src/hooks/use-zod-form.ts
@@ -1,14 +1,14 @@
 import { useForm, type UseFormProps } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import z, { ZodSchema } from "zod";
+import * as z from "zod";
 
-export type ZodFormValues<S extends ZodSchema> = z.infer<S>;
+export type ZodFormValues<S extends z.ZodTypeAny> = z.infer<S>;
 
-interface Props<S extends ZodSchema> extends Omit<UseFormProps<ZodFormValues<S>>, "resolver"> {
+interface Props<S extends z.ZodTypeAny> extends Omit<UseFormProps<ZodFormValues<S>>, "resolver"> {
   schema: S;
 }
 
-export function useZodForm<S extends ZodSchema>(props: Props<S>) {
+export function useZodForm<S extends z.ZodTypeAny>(props: Props<S>) {
   const form = useForm<ZodFormValues<S>>({
     ...props,
     resolver: zodResolver(props.schema),
